5f3e74ecfed3dcfc5b1c72507e46a5313afa0873
Kevin Galinsky <kgalinsky@gmail.com>
2014-04-15 16:45:08 -0400

    tabs to spaces

--- src/fastpca-iterative.c
+++ src/fastpca-iterative.c
@@ -33,144 +33,144 @@ extern int opterr, optopt, optind;
 extern char *optarg;
 
 int main (int argc, char **argv) {
-	parse_args(argc, argv);
+    parse_args(argc, argv);
 
-	FILE *fh_geno = fopen(GENO_FILENAME, "r");
+    FILE *fh_geno = fopen(GENO_FILENAME, "r");
 
-	size_t i;
-	size_t n = kjg_genoIO_num_ind(fh_geno);
-	size_t m = kjg_genoIO_num_snp(fh_geno, n);
+    size_t i;
+    size_t n = kjg_genoIO_num_ind(fh_geno);
+    size_t m = kjg_genoIO_num_snp(fh_geno, n);
 
-	kjg_geno *X  = kjg_geno_alloc(m, n);
-	char *buffer = malloc(sizeof(char)*(n+1));
-	uint8_t *x   = malloc(sizeof(uint8_t)*n);
-	double *y    = malloc(sizeof(double)*n);
-	double *M    = malloc(sizeof(double)*m);
+    kjg_geno *X  = kjg_geno_alloc(m, n);
+    char *buffer = malloc(sizeof(char)*(n+1));
+    uint8_t *x   = malloc(sizeof(uint8_t)*n);
+    double *y    = malloc(sizeof(double)*n);
+    double *M    = malloc(sizeof(double)*m);
 
-	gsl_rng *r    = kjg_rng_init();
-	gsl_matrix *G1 = gsl_matrix_alloc(n, L);
-	gsl_matrix *G2 = gsl_matrix_alloc(n, L);
-	gsl_matrix *H  = gsl_matrix_alloc(m, (I+1)*L);
-	gsl_matrix *X1;
-	gsl_matrix *V1;
-	gsl_vector *S1;
-	gsl_vector *work1;
-	gsl_matrix *Q  = gsl_matrix_alloc(m, (I+1)*L);
-	gsl_matrix *T;
-	gsl_matrix *X2;
-	gsl_matrix *W;
-	gsl_vector *S2;
-	gsl_vector *work2;
+    gsl_rng *r    = kjg_rng_init();
+    gsl_matrix *G1 = gsl_matrix_alloc(n, L);
+    gsl_matrix *G2 = gsl_matrix_alloc(n, L);
+    gsl_matrix *H  = gsl_matrix_alloc(m, (I+1)*L);
+    gsl_matrix *X1;
+    gsl_matrix *V1;
+    gsl_vector *S1;
+    gsl_vector *work1;
+    gsl_matrix *Q  = gsl_matrix_alloc(m, (I+1)*L);
+    gsl_matrix *T;
+    gsl_matrix *X2;
+    gsl_matrix *W;
+    gsl_vector *S2;
+    gsl_vector *work2;
 
-	// PREP - read genotype file into memory
-	kjg_genoIO_fread_geno(buffer, x, X, fh_geno);
-	fclose(fh_geno);
-	free(buffer);
+    // PREP - read genotype file into memory
+    kjg_genoIO_fread_geno(buffer, x, X, fh_geno);
+    fclose(fh_geno);
+    free(buffer);
 
-	// STEP 1A - generate G - O(NL)
-	kjg_matrix_set_ran_ugaussian(G1, r);
-	// kjg_matrix_fprintf(fh_G, G1, "%g");
-	// fclose(fh_G);
+    // STEP 1A - generate G - O(NL)
+    kjg_matrix_set_ran_ugaussian(G1, r);
+    // kjg_matrix_fprintf(fh_G, G1, "%g");
+    // fclose(fh_G);
 
-	// STEP 1B - compute H - O(MN(I+1)L)
-	kjg_geno_row_means(X, M, x);
-	kjg_blanczos(X, M, x, y, H, G1, G2);
-	// kjg_matrix_fprintf(fh_H, H, "%g");
-	// fclose(fh_H);
-	gsl_matrix_free(G2);
+    // STEP 1B - compute H - O(MN(I+1)L)
+    kjg_geno_row_means(X, M, x);
+    kjg_blanczos(X, M, x, y, H, G1, G2);
+    // kjg_matrix_fprintf(fh_H, H, "%g");
+    // fclose(fh_H);
+    gsl_matrix_free(G2);
 
-	FILE *fh_evec = kjg_fopen_suffix(OUTPUT_PREFIX, "evec", "w");
+    FILE *fh_evec = kjg_fopen_suffix(OUTPUT_PREFIX, "evec", "w");
 
-	for (i = 0; i <= I; i += J) {
-		fprintf(fh_evec, "#%d\n", J);
-		// STEP 2P - copy H to Q
-		gsl_matrix_const_view Hv = gsl_matrix_const_submatrix(H, 0, 0, H->size1, (i+1)*L);
-		gsl_matrix_view Qv = gsl_matrix_submatrix(Q, 0, 0, Q->size1, (i+1)*L);
-		gsl_matrix_memcpy(&Qv.matrix, &Hv.matrix);
+    for (i = 0; i <= I; i += J) {
+        fprintf(fh_evec, "#%d\n", J);
+        // STEP 2P - copy H to Q
+        gsl_matrix_const_view Hv = gsl_matrix_const_submatrix(H, 0, 0, H->size1, (i+1)*L);
+        gsl_matrix_view Qv = gsl_matrix_submatrix(Q, 0, 0, Q->size1, (i+1)*L);
+        gsl_matrix_memcpy(&Qv.matrix, &Hv.matrix);
 
-		// STEP 2 - supposed to be pivoted QR, but can't figure it out - O(M[(I+1)L)]^2)
-		X1    = gsl_matrix_alloc(Qv.matrix.size2, Qv.matrix.size2);
-		V1    = gsl_matrix_alloc(Qv.matrix.size2, Qv.matrix.size2);
-		S1    = gsl_vector_alloc(Qv.matrix.size2);
-		work1 = gsl_vector_alloc(Qv.matrix.size2);
-		gsl_linalg_SV_decomp_mod(&Qv.matrix, X1, V1, S1, work1);
-		gsl_matrix_free(X1);
-		gsl_matrix_free(V1);
-		gsl_vector_free(S1);
-		gsl_vector_free(work1);
+        // STEP 2 - supposed to be pivoted QR, but can't figure it out - O(M[(I+1)L)]^2)
+        X1    = gsl_matrix_alloc(Qv.matrix.size2, Qv.matrix.size2);
+        V1    = gsl_matrix_alloc(Qv.matrix.size2, Qv.matrix.size2);
+        S1    = gsl_vector_alloc(Qv.matrix.size2);
+        work1 = gsl_vector_alloc(Qv.matrix.size2);
+        gsl_linalg_SV_decomp_mod(&Qv.matrix, X1, V1, S1, work1);
+        gsl_matrix_free(X1);
+        gsl_matrix_free(V1);
+        gsl_vector_free(S1);
+        gsl_vector_free(work1);
 
-		// STEP 3 - O(MN(I+1)L)
-		T  = gsl_matrix_alloc(n, Qv.matrix.size2);
-		kjg_XTH(X, M, x, y, &Qv.matrix, T);
+        // STEP 3 - O(MN(I+1)L)
+        T  = gsl_matrix_alloc(n, Qv.matrix.size2);
+        kjg_XTH(X, M, x, y, &Qv.matrix, T);
 
-		// STEP 4 - final SVD
-		X2 = gsl_matrix_alloc(T->size2, T->size2);
-		W  = gsl_matrix_alloc(T->size2, T->size2);
-		S2 = gsl_vector_alloc(T->size2);
-		work2 = gsl_vector_alloc(T->size2);
-		gsl_linalg_SV_decomp_mod(T, X2, W, S2, work2);
-		gsl_matrix_free(X2);
-		gsl_vector_free(work2);
-		gsl_matrix_free(W);
+        // STEP 4 - final SVD
+        X2 = gsl_matrix_alloc(T->size2, T->size2);
+        W  = gsl_matrix_alloc(T->size2, T->size2);
+        S2 = gsl_vector_alloc(T->size2);
+        work2 = gsl_vector_alloc(T->size2);
+        gsl_linalg_SV_decomp_mod(T, X2, W, S2, work2);
+        gsl_matrix_free(X2);
+        gsl_vector_free(work2);
+        gsl_matrix_free(W);
 
-		gsl_matrix *V2 = T;
-		gsl_matrix_view V = gsl_matrix_submatrix(V2, 0, 0, V2->size1, K);
-		gsl_vector_view S = gsl_vector_subvector(S2, 0, K);
-		gsl_vector_mul(&S.vector, &S.vector);
+        gsl_matrix *V2 = T;
+        gsl_matrix_view V = gsl_matrix_submatrix(V2, 0, 0, V2->size1, K);
+        gsl_vector_view S = gsl_vector_subvector(S2, 0, K);
+        gsl_vector_mul(&S.vector, &S.vector);
 
-		kjg_evec_fprintf(fh_evec, &S.vector, &V.matrix, "%g");
-		gsl_matrix_free(V2);
-		gsl_vector_free(S2);
-	}
-	kjg_geno_free(X);
-	return(0);
+        kjg_evec_fprintf(fh_evec, &S.vector, &V.matrix, "%g");
+        gsl_matrix_free(V2);
+        gsl_vector_free(S2);
+    }
+    kjg_geno_free(X);
+    return(0);
 }
 
 void parse_args (int argc, char **argv) {
-	int c;
+    int c;
 
-	opterr = 1;
-	while ((c = getopt(argc, argv, "i:k:l:o:")) != -1) {
-		switch (c) {
-		case 'i':
-			I = atoi(optarg);
-			break;
-		case 'j':
-			J = atoi(optarg);
-			break;
-		case 'k':
-			K = atoi(optarg);
-			break;
-		case 'l':
-			L = atoi(optarg);
-			break;
-		case 'o':
-			OUTPUT_PREFIX = optarg;
-			break;
-		default:
-			fprintf(stderr, "Unrecognized option '-%c'\n", optopt);
-			exit(1);
-		}
-	}
+    opterr = 1;
+    while ((c = getopt(argc, argv, "i:k:l:o:")) != -1) {
+        switch (c) {
+        case 'i':
+            I = atoi(optarg);
+            break;
+        case 'j':
+            J = atoi(optarg);
+            break;
+        case 'k':
+            K = atoi(optarg);
+            break;
+        case 'l':
+            L = atoi(optarg);
+            break;
+        case 'o':
+            OUTPUT_PREFIX = optarg;
+            break;
+        default:
+            fprintf(stderr, "Unrecognized option '-%c'\n", optopt);
+            exit(1);
+        }
+    }
 
-	if (K >= L) {
-		fprintf(stderr, "K >= L\n");
-		exit(1);
-	}
+    if (K >= L) {
+        fprintf(stderr, "K >= L\n");
+        exit(1);
+    }
 
-	if (optind == argc - 1) {
-		GENO_FILENAME = argv[optind];
-	}
-	else if (optind == argc) {
-		fprintf(stderr, "No input file specified\n");
-		exit(1);
-	}
-	else {
-		fprintf(stderr, "Too many arguments\n");
-		exit(1);
-	}
+    if (optind == argc - 1) {
+        GENO_FILENAME = argv[optind];
+    }
+    else if (optind == argc) {
+        fprintf(stderr, "No input file specified\n");
+        exit(1);
+    }
+    else {
+        fprintf(stderr, "Too many arguments\n");
+        exit(1);
+    }
 
-	if (OUTPUT_PREFIX == NULL) {
-		OUTPUT_PREFIX = GENO_FILENAME;
-	}
+    if (OUTPUT_PREFIX == NULL) {
+        OUTPUT_PREFIX = GENO_FILENAME;
+    }
 }
--- src/kjg_geno.c
+++ src/kjg_geno.c
@@ -15,181 +15,181 @@
 #include <stdint.h>
 
 double kjg_geno_mean(const uint8_t *x, const size_t n) {
-	size_t i;           // iterator
-	size_t m = 0;       // good genotypes
-	unsigned int t = 0; // sum of genotypes
+    size_t i;           // iterator
+    size_t m = 0;       // good genotypes
+    unsigned int t = 0; // sum of genotypes
 
-	for (i=0; i<n; i++) {
-		if (x[i] < 3) { // genotype not failure
-			t += x[i];
-			m++;
-		}
-	}
+    for (i=0; i<n; i++) {
+        if (x[i] < 3) { // genotype not failure
+            t += x[i];
+            m++;
+        }
+    }
 
-	if (m == 0) {
-		return(-1);
-	}
+    if (m == 0) {
+        return(-1);
+    }
 
-	return(((double) t) / m);
+    return(((double) t) / m);
 }
 
 int kjg_geno_normalization_lookup(const double m, double s[4]) {
-	double p = m / 2;
-	double q = 1 - p;
-	size_t i;
+    double p = m / 2;
+    double q = 1 - p;
+    size_t i;
 
-	s[3] = 0;
+    s[3] = 0;
 
-	if (p == 0 || q == 0) { // check for division by zero
+    if (p == 0 || q == 0) { // check for division by zero
 
-		for (i=0; i<3; i++) {
-			s[i] = 0; // zero out scaling array
-		}
+        for (i=0; i<3; i++) {
+            s[i] = 0; // zero out scaling array
+        }
 
-		return(1); // error
-	}
+        return(1); // error
+    }
 
-	double d = sqrt(2 * p * q); // variance for Binomial(2, p) = 2pq
-	for (i = 0; i < 3; i++) {
-		s[i] = (i - m) / d;
-	}
+    double d = sqrt(2 * p * q); // variance for Binomial(2, p) = 2pq
+    for (i = 0; i < 3; i++) {
+        s[i] = (i - m) / d;
+    }
 
-	return (0);
+    return (0);
 }
 
 int kjg_geno_normalize(const uint8_t *x, double *y, const size_t n) {
-	double m = kjg_geno_mean(x, n);
-	int r = kjg_geno_normalize_m(m, x, y, n);
-	return(r);
+    double m = kjg_geno_mean(x, n);
+    int r = kjg_geno_normalize_m(m, x, y, n);
+    return(r);
 }
 
 int kjg_geno_normalize_m(const double m, const uint8_t* x, double* y,
-		const size_t n) {
-	double s[4];
-	int r = kjg_geno_normalization_lookup(m, s);
-	kjg_geno_remap(s, x, y, n);
-	return r;
+        const size_t n) {
+    double s[4];
+    int r = kjg_geno_normalization_lookup(m, s);
+    kjg_geno_remap(s, x, y, n);
+    return r;
 }
 
 void kjg_geno_remap(const double s[4], const uint8_t* x, double* y,
-		const size_t n) {
-	size_t i;
-	for (i = 0; i < n; i++) {
-		y[i] = s[x[i]];
-	}
+        const size_t n) {
+    size_t i;
+    for (i = 0; i < n; i++) {
+        y[i] = s[x[i]];
+    }
 }
 
 int kjg_geno_snp_correlation(const uint8_t* x, double* C, const size_t n) {
-	double m = kjg_geno_mean(x, n);
+    double m = kjg_geno_mean(x, n);
 
-	double s[4];
-	int r = kjg_geno_normalization_lookup(m, s);
+    double s[4];
+    int r = kjg_geno_normalization_lookup(m, s);
 
-	double S[4][4];
-	kjg_geno_correlation_lookup(s, S);
+    double S[4][4];
+    kjg_geno_correlation_lookup(s, S);
 
-	size_t i, j, ni;
-	double l;
-	uint8_t xi;
-	for (i = 0; i < n; i++) {
-		ni = n * i;
-		xi = x[i];
-		C[ni+i] += S[xi][xi];
-		for (j = 0; j < i; j++) {
-			l = S[xi][x[j]];
-			C[ni+j] += l;
-		}
-	}
-	return (r);
+    size_t i, j, ni;
+    double l;
+    uint8_t xi;
+    for (i = 0; i < n; i++) {
+        ni = n * i;
+        xi = x[i];
+        C[ni+i] += S[xi][xi];
+        for (j = 0; j < i; j++) {
+            l = S[xi][x[j]];
+            C[ni+j] += l;
+        }
+    }
+    return (r);
 }
 
 void kjg_geno_correlation_lookup(const double s[4], double S[4][4]) {
-	size_t i, j;
-	for (i = 0; i < 3; i++) {
-		S[i][3] = 0;
-		S[3][i] = 0;
-		for (j = 0; j < 3; j++) {
-			S[i][j] = S[j][i] = s[i] * s[j];
-		}
-	}
+    size_t i, j;
+    for (i = 0; i < 3; i++) {
+        S[i][3] = 0;
+        S[3][i] = 0;
+        for (j = 0; j < 3; j++) {
+            S[i][j] = S[j][i] = s[i] * s[j];
+        }
+    }
 }
 
 double* kjg_geno_correlation_matrix_init(const size_t n) {
-	size_t i, n2 = n*n;
-	double* C = malloc(sizeof(double) * n2);
-	for (i = 0; i < n2; i++) {
-		C[i] = 0;
-	}
-	return (C);
+    size_t i, n2 = n*n;
+    double* C = malloc(sizeof(double) * n2);
+    for (i = 0; i < n2; i++) {
+        C[i] = 0;
+    }
+    return (C);
 }
 
 void kjg_geno_correlation_matrix_finish(double* C, const size_t n) {
-	size_t i, j, ni;
-	for (i = 1; i < n; i++) {
-		ni = n*i;
-		for (j = i+1; j < n; j++) {
-			C[ni+j] = C[j*n+i];
-		}
-	}
+    size_t i, j, ni;
+    for (i = 1; i < n; i++) {
+        ni = n*i;
+        for (j = i+1; j < n; j++) {
+            C[ni+j] = C[j*n+i];
+        }
+    }
 }
 
 void kjg_geno_pack(const uint8_t* x, uint8_t* y, size_t n) {
-	size_t i, j = 0;
-	uint8_t* p;
-	for (i = 0; i < n; i ++) {
-		if (i % 4 == 0) {
-			p = y + j++;
-			*p = 0;
-		} else {
-			*p <<= 2;
-		}
-		*p |= x[i];
-	}
+    size_t i, j = 0;
+    uint8_t* p;
+    for (i = 0; i < n; i ++) {
+        if (i % 4 == 0) {
+            p = y + j++;
+            *p = 0;
+        } else {
+            *p <<= 2;
+        }
+        *p |= x[i];
+    }
 }
 
 void kjg_geno_unpack(uint8_t* x, const uint8_t* y, size_t n) {
-	size_t i, j = n/4;
-	uint8_t p;
+    size_t i, j = n/4;
+    uint8_t p;
 
-	if (n % 4 != 0) {
-		p = y[j] << 2;
-	}
+    if (n % 4 != 0) {
+        p = y[j] << 2;
+    }
 
-	for (i = n; i > 0; ) {
-		if (i % 4 == 0) {
-			p = y[--j];
-		} else {
-			p >>= 2;
-		}
-		x[--i] = p & 3;
-	}
+    for (i = n; i > 0; ) {
+        if (i % 4 == 0) {
+            p = y[--j];
+        } else {
+            p >>= 2;
+        }
+        x[--i] = p & 3;
+    }
 }
 
 void kjg_geno_free(kjg_geno* g) {
-	free(g->data);
-	free(g);
+    free(g->data);
+    free(g);
 }
 
 kjg_geno* kjg_geno_alloc(size_t m, size_t n) {
-	kjg_geno* g = malloc(sizeof(kjg_geno));
-	g->m = m;
-	g->n = n;
-	g->tda = (n + 3) / 4;
-	g->data = malloc(sizeof(uint8_t) * g->m * g->tda);
-	return (g);
+    kjg_geno* g = malloc(sizeof(kjg_geno));
+    g->m = m;
+    g->n = n;
+    g->tda = (n + 3) / 4;
+    g->data = malloc(sizeof(uint8_t) * g->m * g->tda);
+    return (g);
 }
 
 void kjg_geno_get_row(uint8_t* x, const kjg_geno* g, const size_t i) {
-	kjg_geno_unpack(x, g->data + g->tda * i, g->n);
+    kjg_geno_unpack(x, g->data + g->tda * i, g->n);
 }
 
 void kjg_geno_set_row(const uint8_t* x, kjg_geno* g, const size_t i) {
-	kjg_geno_pack(x, g->data + (g->tda * i), g->n);
+    kjg_geno_pack(x, g->data + (g->tda * i), g->n);
 }
 void kjg_geno_row_means(const kjg_geno* g, double* M, uint8_t* x) {
-	size_t i;
-	for (i = 0; i < g->m; i++) {
-		kjg_geno_get_row(x, g, i);
-		M[i] = kjg_geno_mean(x, g->n);
-	}
+    size_t i;
+    for (i = 0; i < g->m; i++) {
+        kjg_geno_get_row(x, g, i);
+        M[i] = kjg_geno_mean(x, g->n);
+    }
 }
