20b9269f931cb282cad9131c4a6ec455b060245d
Kevin Galinsky <galinsky@fas.harvard.edu>
2014-04-22 16:41:09 -0400

    Set bounds of uniform to [0.1,0.9]. Allow numbers to be passed with "k" signifying thousands. Calculate F only one time. Truncate the beta distributions to avoid rounding issues.

--- scripts/rand_pop.pl
+++ scripts/rand_pop.pl
@@ -9,29 +9,48 @@ use Math::Random qw/
   random_binomial
   /;
 
-my $unif_low  = 0.05;
-my $unif_high = 0.95;
+my $unif_low  = 0.1;
+my $unif_high = 0.9;
 
 my $m = $ARGV[0];
+$m =~ s/k/000/;
 
 my $struct;
+$ARGV[1] =~ s/k/000/g;
 eval "\$struct = $ARGV[1];";
+die $@ if ($@);
+
+sub calc_F {
+    my ($struct) = @_;
+    if ( ref $struct ) {
+        my $FST = $struct->[0];
+        my $F   = ( 1 - $FST ) / $FST;
+        $struct->[0] = $F;
+
+        foreach my $substruct (@$struct[1..$#$struct]) {
+	    calc_F($substruct);
+        }
+    }
+}
+
+calc_F($struct);
 
 sub random_pop {
     my ( $p, $struct ) = @_;
 
     if ( ref $struct ) {
-        my $FST = $struct->[0];
-        my $F   = ( 1 - $FST ) / $FST;
-
+        my $F  = $struct->[0];
         my $aa = $p * $F;
         my $bb = ( 1 - $p ) * $F;
 
         return (
             [
                 $p,
-                map { random_pop( random_beta( 1, $aa, $bb ), $struct->[$_] ) }
-                  ( 1 .. $#$struct )
+                map {
+                    my $p = 0;
+                    while ($p < 0.01 || $p > 0.99) { $p = random_beta( 1, $aa, $bb ) }
+                    random_pop( $p, $struct->[$_] )
+                } ( 1 .. $#$struct )
             ]
         );
     }
