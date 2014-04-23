ac371cf7ced1961341af39d530dc3ebe4d93500b
Kevin Galinsky <kgalinsky@gmail.com>
2014-04-23 17:21:41 -0400

    polar box muller algorithm

--- src/kjg_rand.c
+++ src/kjg_rand.c
@@ -14,19 +14,27 @@ double kjg_runif() { return((double)rand() / RAND_MAX); }
 
 void kjg_rnorms(size_t n, double *Z) {
     size_t i;
-
-    // Box-Muller algorithm generates 2 at a time
+    double x, y, r2;
+    // Polar Box-Muller algorithm generates 2 at a time
     for (i = 0; i < n - 1; i += 2) {
-        double R = sqrt(-2 * log(kjg_runif()));
-        double T = 2 * M_PI * kjg_runif();
-        Z[i]   = R*cos(T);
-        Z[i+1] = R*sin(T);
+        do {
+            x = -1 + 2*kjg_runif();
+            y = -1 * 2*kjg_runif();
+            r2 = x*x + y*y;
+        } while (r2 > 1.0 || r2 == 0);
+        r2 = sqrt(-2*log(r2)/r2);
+        Z[i]   = x * r2;
+        Z[i+1] = y * r2;
     }
 
     // If n is odd
     if (i != n) {
-        double R = sqrt(-2 * log(kjg_runif()));
-        double T = 2 * M_PI * kjg_runif();
-        Z[i]   = R*cos(T);
+        do {
+            x = -1 + 2*kjg_runif();
+            y = -1 * 2*kjg_runif();
+            r2 = x*x + y*y;
+        } while (r2 > 1.0 || r2 == 0);
+        r2 = sqrt(-2*log(r2)/r2);
+        Z[i]   = x * r2;
     }
 }
