e9aa97afa17116aaa8e29687750dd7e6e413f84b
Kevin Galinsky <kgalinsky@gmail.com>
2014-04-23 12:32:39 -0400

    timer function

--- src/kjg_util.h
+++ src/kjg_util.h
@@ -8,8 +8,6 @@
 #ifndef KJG_UTIL_H_
 #define KJG_UTIL_H_
 
-#include <stdio.h>
-
 /**
  * Open a file given a prefix and suffix.
  *
@@ -19,5 +17,16 @@
  */
 
 FILE* kjg_fopen_suffix (const char* prefix, const char* suffix, const char* opentype);
+
+
+/**
+ * Return number of seconds elapsed from two timespec structs
+ *
+ * @param const struct timespec t1 - start time
+ * @param const struct timespec t2 - end time
+ * @return double - seconds elapsed
+ */
+
+double kjg_deltat(const struct timespec t1, const struct timespec t2);
 
 #endif /* KJG_UTIL_H_ */
