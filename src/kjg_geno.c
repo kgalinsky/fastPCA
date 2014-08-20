0b6d3fe44ad9683e16ba2c2e80ce82cc0a88561c
Kevin Galinsky <kgalinsky@gmail.com>
2014-08-19 21:01:07 -0400

    Vectorized code to get a normalized row. Use memset for initializing zero rows and normalization tables.

--- src/kjg_geno.c
+++ src/kjg_geno.c
@@ -54,39 +54,41 @@ void kjg_geno_set_row (kjg_geno* g, const size_t i, const uint8_t* x) {
 #define M3(n)   M2(n), M2(n | (1 << 4)), M2(n | (2 << 4)), M2(n | (3 << 4))
 
 // mean lookup array
-static const uint8_t MEAN_LOOKUP[256] =
-    { M3(0), M3((1 << 6)), M3((2 << 6)), M3((3 << 6)) };
+static const uint8_t MEAN_LOOKUP[256] = { M3(0), M3((1 << 6)), M3((2 << 6)), M3(
+        (3 << 6)) };
 
 double kjg_geno_row_mean (const kjg_geno* g, const size_t i) {
     size_t tda = g->tda;
     uint8_t* p = g->data + (g->tda * i);
     size_t sum = 0;
-    while (tda--) sum += MEAN_LOOKUP[*(p++)];
-    return( ((double) sum) / g->n);
+    while (tda--)
+        sum += MEAN_LOOKUP[*(p++)];
+    return (((double) sum) / g->n);
 }
 
 void kjg_geno_row_means (const kjg_geno* g, double* M) {
     size_t i;
-    for (i = 0; i < g->m; i++) M[i] = kjg_geno_row_mean(g, i);
+    for (i = 0; i < g->m; i++)
+        M[i] = kjg_geno_row_mean(g, i);
 }
 
 // Normalization
 
 int kjg_geno_normalization_lookup (const double m, double s[4]) {
-    size_t i;
-    s[3] = 0;
-
     if (m == 0 || m == 2) {             // check for homogeneous population
-        for (i = 0; i < 3; i++)
-            s[i] = 0;                   // zero out scaling array
+        memset(s, 0, 4);                // zero out scaling array
         return (1);                     // error
     }
+
+    size_t i;
 
     double p = m / 2;                   // G ~ Binomial(2, p)
     double d = sqrt(2 * p * (1 - p));   // Var(G) = 2pq
 
     for (i = 0; i < 3; i++)
         s[i] = (i - m) / d;
+
+    s[3] = 0;
 
     return (0);
 }
@@ -97,20 +99,27 @@ void kjg_geno_get_normalized_row (
         const size_t i,
         double* y) {
     double s[4];
-    kjg_geno_normalization_lookup(M[i], s);
+    size_t j = kjg_geno_normalization_lookup(M[i], s);
+
+    if (j) {
+        memset(y, 0, sizeof(double) * g->n);
+        return;
+    }
 
     size_t tda = g->tda;
     uint8_t* p = g->data + (g->tda * i);
 
-    size_t j;
-
-    while (--tda) {
-        const uint8_t* u = KJG_2BIT_UNPACK_LOOKUP[*(p++)];
-        for (j = 0; j < 4; j++) *(y++) = s[*(u++)];
+    for (j = 0; j < g->tda - 1; j++) {
+        const uint8_t* u = KJG_2BIT_UNPACK_LOOKUP[p[j]];
+        y[4 * j] = s[u[0]];
+        y[4 * j + 1] = s[u[1]];
+        y[4 * j + 2] = s[u[2]];
+        y[4 * j + 3] = s[u[3]];
     }
 
-    const uint8_t* u = KJG_2BIT_UNPACK_LOOKUP[*p];
-    for (j = (g->tda - 1 ) * 4; j < g->n; j++) *(y++) = s[*(u++)];
+    const uint8_t* u = KJG_2BIT_UNPACK_LOOKUP[p[j]];
+    for (j = (g->tda - 1) * 4; j < g->n; j++)
+        y[j] = s[*(u++)];
 }
 
 size_t kjg_geno_get_normalized_rows (
