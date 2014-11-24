962d3cacef82b5e420aacfbda6827072bd8fc350
Kevin Galinsky <kgalinsky@gmail.com>
2014-11-24 11:31:07 -0500

    added support for correlated populations

--- src/sim_star.c
+++ src/sim_star.c
@@ -19,10 +19,13 @@ void
 write_fam (char* prefix, size_t n, size_t p);
 void
 write_bed (char* prefix, size_t M, size_t N, size_t P, double FST, double *MAF);
+void
+write_bed_pair (char* prefix, size_t M, size_t N, size_t P, double FST,
+                double *MAF, double *cor);
 
 size_t opt_M = 10000, opt_N = 1000, opt_P = 2;
 double opt_FST = 0.01, opt_MAF[2] =
-  { 0.05, 0.5 };
+  { 0.05, 0.5 }, *opt_cor = NULL;
 char* opt_PFX = NULL;
 
 void
@@ -32,7 +35,10 @@ main (int argc, char** argv)
 
   write_bim (opt_PFX, opt_M);
   write_fam (opt_PFX, opt_N, opt_P);
-  write_bed (opt_PFX, opt_M, opt_N, opt_P, opt_FST, opt_MAF);
+  if (opt_cor)
+    write_bed_pair (opt_PFX, opt_M, opt_N, opt_P, opt_FST, opt_MAF, opt_cor);
+  else
+    write_bed (opt_PFX, opt_M, opt_N, opt_P, opt_FST, opt_MAF);
 }
 
 void
@@ -46,11 +52,12 @@ parse_args (int argc, char** argv)
       { "FST", required_argument, 0, 'f' },
       { "MAF", required_argument, 0, 'a' },
       { "prefix", required_argument, 0, 'p' },
+      { "cor", required_argument, 0, 'r' },
       { 0, 0, 0, 0 } };
 
   int help_flag = 0, c, option_index = 0;
 
-  while ((c = getopt_long (argc, argv, "m:n:f:a:p:", long_options,
+  while ((c = getopt_long (argc, argv, "m:n:f:a:p:r:", long_options,
                            &option_index)) != -1)
     {
       switch (c)
@@ -81,7 +88,7 @@ parse_args (int argc, char** argv)
 
           //MAF
         case 'a':
-          sscanf (optarg, "%g-%g", &opt_MAF[0], &opt_MAF[1]);
+          sscanf (optarg, "%lg-%lg", &opt_MAF[0], &opt_MAF[1]);
           if ((opt_MAF[0] < 0) || (opt_MAF[0] > opt_MAF[1])
               || (opt_MAF[1] > 0.5))
             {
@@ -95,6 +102,11 @@ parse_args (int argc, char** argv)
           asprintf (&opt_PFX, "%s", optarg);
           break;
 
+          //Correlation
+        case 'r':
+          opt_cor = malloc (2 * sizeof(double));
+          int err = sscanf (optarg, "%lg,%lg", &opt_cor[0], &opt_cor[1]);
+          break;
         case '?':
           break;
         default:
@@ -124,8 +136,12 @@ parse_args (int argc, char** argv)
     }
 
   if (opt_PFX == NULL)
-    asprintf (&opt_PFX, "star.%dx%dx%d_%g_%g-%g", opt_M, opt_N, opt_P, opt_FST,
-              opt_MAF[0], opt_MAF[1]);
+    if (opt_cor)
+      asprintf (&opt_PFX, "star.%dx%dx%d_%g_%g-%g_%g,%g", opt_M, opt_N, opt_P,
+                opt_FST, opt_MAF[0], opt_MAF[1], opt_cor[0], opt_cor[1]);
+    else
+      asprintf (&opt_PFX, "star.%dx%dx%d_%g_%g-%g", opt_M, opt_N, opt_P,
+                opt_FST, opt_MAF[0], opt_MAF[1]);
 }
 
 void
@@ -157,7 +173,7 @@ void
 write_bed (char* prefix, size_t M, size_t N, size_t P, double FST, double *MAF)
 {
   FILE* fp = kjg_util_fopen_suffix (prefix, "bed", "w");
-
+  FILE* ld = kjg_util_fopen_suffix (prefix, "ld", "w");
   char magic[3] =
     { 0x6c, 0x1b, 0x01 };
   fwrite (magic, 1, 3, fp);
@@ -189,4 +205,51 @@ write_bed (char* prefix, size_t M, size_t N, size_t P, double FST, double *MAF)
   free (x);
 
   fclose (fp);
+}
+
+void
+write_bed_pair (char* prefix, size_t M, size_t N, size_t P, double FST,
+                double *MAF, double *cor)
+{
+  FILE* fp = kjg_util_fopen_suffix (prefix, "bed", "w");
+  FILE* ld = kjg_util_fopen_suffix (prefix, "ldbed", "w");
+
+  char magic[3] =
+    { 0x6c, 0x1b, 0x01 };
+  fwrite (magic, 1, 3, fp);
+  fwrite (magic, 1, 3, ld);
+
+  gsl_rng_env_setup ();
+
+  const gsl_rng_type * T = gsl_rng_default;
+  gsl_rng * r = gsl_rng_alloc (T);
+
+  double F = (1 - FST) / FST;
+
+  size_t n = P * N;
+  size_t tda = (n + 3) / 4;
+  double* AF = malloc (P * sizeof(double));
+  uint8_t* x = malloc (n * sizeof(uint8_t));
+  uint8_t* y = malloc (n * sizeof(uint8_t));
+  uint8_t* p = malloc (tda * sizeof(uint8_t));
+
+  size_t m, j, k, l;
+  for (m = 0; m < M; m++)
+    {
+      double anc = kjg_geno_rand_anc (r, MAF);
+      kjg_geno_rand_star_AF (r, AF, anc, F, P);
+      kjg_geno_rand_row (r, x, N, P, AF);
+      kjg_geno_rand_ld_row (r, y, N, P, AF, x, cor);
+      kjg_geno_pack (n, x, p);
+      fwrite (p, 1, tda, fp);
+      kjg_geno_pack (n, y, p);
+      fwrite (p, 1, tda, ld);
+    }
+
+  free (AF);
+  free (x);
+  free (y);
+
+  fclose (fp);
+  fclose (ld);
 }
